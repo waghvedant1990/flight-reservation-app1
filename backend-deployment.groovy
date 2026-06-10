0dad0920a6425d1d3788307dd6d0b9fd48a0ea2e
vedant <vedantwagh1990@mail.com>
2026-06-10 09:00:24 +0530

    vwagh | git update

--- backend-deployment.groovy
+++ backend-deployment.groovy
@@ -10,7 +10,7 @@ pipeline{
     stages{
         stage('checkout'){
             steps{
-                 git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git' 
+                 git branch: 'main', url: 'https://github.com/waghvedant1990/flight-reservation-app1.git' 
             }
 
         }
